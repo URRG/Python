424f24da17168d0d991a1f86dad11647d222d61e
Utkarsh Rastogi <95937956+URRG@users.noreply.github.com>
2023-04-08 21:57:00 +0530

    Calculator using tkinter gui

---
+++ Calculaor.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+calci = Tk()
+calci.title("Simple Calculator")
+calci.geometry("270x150")
+glb=0
+
+def add:
+    a=(int)txt1
+    b=(int)txt2
+    c=a+b
+    glb=c
+
+def subs:
+    a=(int)txt1
+    b=(int)txt2
+    c=a-b
+    glb=c
+
+def multiply:
+    a=(int)txt1
+    b=(int)txt2
+    c=a*b
+    glb=c
+
+def div:
+    a=(int)txt1
+    b=(int)txt2
+    c=a/b
+    glb=c
+
+
